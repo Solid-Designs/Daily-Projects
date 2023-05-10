e77bb83d57e14dcd242191e400ada1e1c79e3b9e
Christopher Scott <solid.moose15@gmail.com>
2023-05-10 10:29:02 -0500

    may 9 2023

---
+++ may_9_2023/app.py
@@ -0,0 +1,27 @@
+# I learned about lists, and lambda functions
+# list sorting
+# inventory = [
+#     ('Product1', 20),
+#     ('Product2', 10),
+#     ('Product3', 30)
+# ]
+
+
+# def sort_price(inventory_list):
+#     return inventory_list[1]
+
+
+# inventory.sort(key=sort_price)
+# print(inventory)
+
+
+# lambda functions
+# inventory = [
+#     ('Product1', 20),
+#     ('Product2', 10),
+#     ('Product3', 30)
+# ]
+
+
+# inventory.sort(key=lambda inventory_list: inventory_list[1])
+# print(inventory)
