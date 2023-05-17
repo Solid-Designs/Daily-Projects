0e8c92c075f07b015e50012b774a757e9a7b4434
Christopher Scott <solid.moose15@gmail.com>
2023-05-17 12:40:37 -0500

    may_17_2023

---
+++ may_17_2023/app.py
@@ -0,0 +1,11 @@
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+
+cloud = TagCloud()
+cloud.add('python')
+print(cloud.tags)
